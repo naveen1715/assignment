68b78edef6ba1862f96627a95c2dc6a41865be08
naveen <malladinaveen1999@gmail.com>
2024-01-30 21:08:53 -0600

    intializes amount

--- src/method/BankDeposit.java
+++ src/method/BankDeposit.java
@@ -2,10 +2,12 @@ package method;
 
 public class BankDeposit {
 	int balance;
+	int amount;
+
 	public void depositAmount(int depositAmount) {
-		
+
 		balance = balance + depositAmount;
-		
+
 	}
 
 }
